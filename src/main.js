04f39ff9d0f1575d650a943ed9b9ef28b54be917
Juan <juanmetchart@gmail.com>
2020-03-29 23:15:41 +0200

    Node version on package.json

--- src/main.js
+++ src/main.js
@@ -4,21 +4,11 @@ import store from './store/store'
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import Vuetify from 'vuetify'
-import * as io from 'socket.io-client'
-import VueSocketIO from 'vue-socket.io'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
-
-/*Vue.use(new VueSocketIO({
-  debug: true,
-  connection: 'http://localhost:3000',
-}));*/
-
-// Install BootstrapVue
 Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
 
