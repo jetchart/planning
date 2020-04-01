404858e57d0fdb96dba7491e37ef0ee379fcbbc8
Juan <juanmetchart@gmail.com>
2020-04-01 03:48:30 +0200

    Socket url

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,8 +20,8 @@
         user: null,
         user: {},
         url: process.env.VUE_APP_URL_EXPRESS_SERVER || 'localhost:3000',
-        //socket : io('https://planning-vue.herokuapp.com/'),
-        socket : io('localhost:3000'),
+        socket : io('https://planning-vue.herokuapp.com/'),
+        //socket : io('localhost:3000'),
         options: [0.5, 1, 2, 3, 5]
       }
     },
